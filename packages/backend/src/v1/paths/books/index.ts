2e5a4418c56e7686f489f5b5d2e45f3a40913c8f
David Taing <adavidtaing@gmail.com>
2022-01-16 20:23:42 +1100

    add stub handlers for /books endpoint

---
+++ packages/backend/src/v1/paths/books/index.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { Operation } from "express-openapi";
+import { paths } from "../../openapi/spec";
+
+// GET /books
+const GET: Operation = (req: Request, res: Response, next: NextFunction) => {
+  res.status(501).json();
+};
+
+// POST /books
+const POST: Operation = [
+  (req: Request, res: Response, next: NextFunction) => {
+    res.status(501).json();
+  },
+];
+
+GET.apiDoc = paths["/books"]?.get;
+POST.apiDoc = paths["/books"]?.post;
+
+const operations = () => ({ GET, POST });
+
+export default operations;
