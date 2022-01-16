71c4f141c415dd3e4721d15070170050a98b64d4
David Taing <adavidtaing@gmail.com>
2022-01-16 21:00:33 +1100

    implement stub handlers for /members/:memberId

---
+++ packages/backend/src/v1/paths/members/{memberId}.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import { Operation } from "express-openapi";
+import { paths } from "../../openapi/spec";
+
+// GET /members/:memberId
+const GET: Operation = [
+  (req: Request, res: Response, next: NextFunction) => {
+    res.status(501).json();
+  },
+];
+
+// PATCH /members/:memberId
+const PATCH: Operation = [
+  (req: Request, res: Response, next: NextFunction) => {
+    res.status(501).json();
+  },
+];
+
+// DELETE /members/:memberId
+const DELETE: Operation = [
+  (req: Request, res: Response, next: NextFunction) => {
+    res.status(501).json();
+  },
+];
+
+GET.apiDoc = paths["/members/:memberId"]?.get;
+PATCH.apiDoc = paths["/members/:memberId"]?.patch;
+DELETE.apiDoc = paths["/members/:memberId"]?.delete;
+
+const operations = () => ({ GET, PATCH, DELETE });
+
+export default operations;
