ef8750008c1b1f6d50956d168cb88f6c4daa24e6
David Taing <adavidtaing@gmail.com>
2022-01-16 20:44:23 +1100

    init and configure swagger-ui-express

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import helmet from "helmet";
 import compression from "compression";
 import { initialize } from "express-openapi";
+import swaggerUi from "swagger-ui-express";
 import apiSpec from "./v1/openapi/spec";
 
 const app = express();
@@ -16,7 +17,19 @@ initialize({
   app,
   apiDoc: apiSpec,
   paths: "./src/v1/paths",
+  routesGlob: "**/*.{ts,js}",
+  routesIndexFileRegExp: /(?:index)?\.[tj]s$/,
 });
+
+app.use(
+  "/openapi",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "http://localhost:3000/api-docs",
+    },
+  })
+);
 
 app.listen(3000, () => {
   console.log(`Express Server Listening on ${PORT}`);
