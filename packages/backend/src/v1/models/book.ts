c8bce8335b8094c936e5f01c37ba8bbdc30340c6
David Taing <adavidtaing@gmail.com>
2022-01-16 22:08:16 +1100

    implement BookModel

---
+++ packages/backend/src/v1/models/book.ts
@@ -0,0 +1,18 @@
+import { model, Schema, Types } from "mongoose";
+import { Book } from "../../../../openapi/v1/components/schemas";
+
+export const BookSchema = new Schema<Book>({
+  isbn10: { type: String, required: true },
+  isbn13: { type: String, required: true },
+  title: { type: String, required: true },
+  subtitle: { type: String, required: true },
+  author: { type: String, required: true },
+  publisher: { type: String, required: true },
+  category: { type: String, required: true },
+  quantityAvailable: { type: Number, required: true },
+  borrowers: [{ type: Types.ObjectId, ref: "Member" }],
+});
+
+const BookModel = model("Book", BookSchema);
+
+export default BookModel;
