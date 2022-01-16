eead6795ee33f02c62e3741d613fd9560e760edf
David Taing <adavidtaing@gmail.com>
2022-01-16 20:52:34 +1100

    refactor GET /books operation into array

--- packages/backend/src/v1/paths/books/index.ts
+++ packages/backend/src/v1/paths/books/index.ts
@@ -3,9 +3,11 @@ import { Operation } from "express-openapi";
 import { paths } from "../../openapi/spec";
 
 // GET /books
-const GET: Operation = (req: Request, res: Response, next: NextFunction) => {
-  res.status(501).json();
-};
+const GET: Operation = [
+  (req: Request, res: Response, next: NextFunction) => {
+    res.status(501).json();
+  },
+];
 
 // POST /books
 const POST: Operation = [
