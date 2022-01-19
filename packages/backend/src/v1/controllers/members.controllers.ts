b5136ad141851f3fc2f449be687c8b6c030f3807
David Taing <adavidtaing@gmail.com>
2022-01-19 12:31:05 +1100

    implement members controllers

--- packages/backend/src/v1/controllers/members.controllers.ts
+++ packages/backend/src/v1/controllers/members.controllers.ts
@@ -1,33 +1,73 @@
 import { Request, Response, NextFunction } from "express";
+import MemberModel from "../models/member";
 
-export const getMembers = (req: Request, res: Response, next: NextFunction) => {
-  res.status(501).json();
-};
-
-export const getMemberById = (
+export const getMembers = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(501).json();
+  try {
+    const filter = req.query;
+    const result = await MemberModel.find(filter);
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const postMember = (req: Request, res: Response, next: NextFunction) => {
-  res.status(501).json();
-};
-
-export const patchMember = (
+export const getMemberById = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(501).json();
+  try {
+    const _id = req.params._id;
+    const result = await MemberModel.findOne({ _id });
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const deleteMember = (
+export const postMember = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(501).json();
+  try {
+    const payload = req.body;
+    const result = await MemberModel.create(payload);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const patchMember = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const _id = req.params._id;
+    const payload = req.body;
+    const result = await MemberModel.findOneAndUpdate({ _id }, payload);
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteMember = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const _id = req.params._id;
+    const result = await MemberModel.findByIdAndDelete({ _id });
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
 };
