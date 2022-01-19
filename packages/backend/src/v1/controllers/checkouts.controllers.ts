ecff16910fa18a3d08ca91f7586c91c3777021ef
David Taing <adavidtaing@gmail.com>
2022-01-19 13:21:03 +1100

    implement checkout controller

--- packages/backend/src/v1/controllers/checkouts.controllers.ts
+++ packages/backend/src/v1/controllers/checkouts.controllers.ts
@@ -1,5 +1,16 @@
 import { Request, Response, NextFunction } from "express";
+import CheckoutModel from "../models/checkout";
 
-export const checkout = (req: Request, res: Response, next: NextFunction) => {
-  res.status(501).json();
+export const checkout = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const payload = req.body;
+    const result = await CheckoutModel.create(payload);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
 };
