b8ecdc0d63514faae6a5ea1c2af0b38caa4d610b
David Taing <adavidtaing@gmail.com>
2022-01-19 18:55:33 +1100

    add dotenv configs

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,13 +1,15 @@
+import dotenv from "dotenv";
 import app from "./app";
 import mongoose from "mongoose";
 
-const expressPort = 3000;
-const dbUrl = "mongodb://localhost:27017/library";
+dotenv.config();
 
-mongoose.connect(dbUrl).then(() => {
-  console.log(`Connected to database at ${dbUrl}`);
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+  console.log(
+    `Connected to database at ${process.env.MONGODB_CONNECTION_STRING}`
+  );
 });
 
-app.listen(expressPort, () => {
-  console.log(`Express Server Listening on ${expressPort}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Express Server Listening on ${process.env.PORT}`);
 });
