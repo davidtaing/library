94ec88faa30758e1f0778a6d33f38d31628ab282
David Taing <adavidtaing@gmail.com>
2022-01-19 13:49:37 +1100

    re-export Book schema interface in backend package

--- packages/backend/src/v1/models/book.ts
+++ packages/backend/src/v1/models/book.ts
@@ -1,7 +1,9 @@
 import { model, Schema, Types } from "mongoose";
 import { Book } from "../../../../openapi/v1/components/schemas";
 
-export const BookSchema = new Schema<Book>({
+export interface BookDocument extends Book {}
+
+export const BookSchema = new Schema<BookDocument>({
   isbn10: { type: String, required: true, unique: true, index: true },
   isbn13: { type: String, required: true, unique: true },
   title: { type: String, required: true },
