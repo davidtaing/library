7598fb8cae34ccf575e799094112ea3ff5f29537
David Taing <adavidtaing@gmail.com>
2022-01-19 14:02:45 +1100

    adjusted CheckoutDocument and Model properties

--- packages/backend/src/v1/models/checkout.ts
+++ packages/backend/src/v1/models/checkout.ts
@@ -1,23 +1,25 @@
 import { Schema, Date, Types, model } from "mongoose";
 import { Checkout } from "../../../../openapi/v1/components/schemas";
-import { BookSchema } from "./book";
+import { MemberDocument } from "./member";
+import { BookDocument } from "./book";
 
 // convert @airtasker/spot date to string and string to objectId
-interface CheckoutDocument extends Omit<Document, "date" | "member"> {
+interface CheckoutDocument
+  extends Omit<Checkout, "date" | "dueDate" | "member" | "books"> {
   date: Date;
-  member: {
-    _id: Types.ObjectId;
-    preferredName: string;
-  };
+  dueDate: Date;
+  member: MemberDocument;
+  books: BookDocument[];
 }
 
-const CheckoutSchema = new Schema<Checkout>({
+const CheckoutSchema = new Schema<CheckoutDocument>({
   date: Date,
+  dueDate: Date,
   member: {
-    _id: Types.ObjectId,
-    preferredName: { type: String },
+    type: Schema.Types.ObjectId,
+    ref: "Member",
   },
-  books: BookSchema,
+  books: [{ type: Schema.Types.ObjectId, ref: "Book" }],
 });
 
 const CheckoutModel = model("Checkout", CheckoutSchema);
--- packages/openapi/v1/components/schemas/checkout.ts
+++ packages/openapi/v1/components/schemas/checkout.ts
@@ -3,6 +3,7 @@ import { Book } from "./";
 
 export interface Checkout {
   date: Date;
+  dueDate: Date;
   member: {
     _id: String;
     preferredName: String;
